016ee28a2ae5e982839798a176f188719194f099
CaseyRay <thehumancasey@gmail.com>
2023-01-16 22:44:30 -0800

    Update trimRect.js

--- src/trimRect.js
+++ src/trimRect.js
@@ -1,3 +1,6 @@
+//from a point in rect insode ploygon to a poiny ourside polygon push intersection to line atray. 
+// push all points in pilygon regardless
+
 //bathroom above the bench
 //let verticesArray = [0, 0, 839, 0, 848, 1860, 0, 1854];
 export function trimRect(rect) {
